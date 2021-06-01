e2c73be9a75170e77bb03bcc3e700560ff026134
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-01 11:58:20 +0200

    Add clear method on submit form

--- src/components/TimelineElementForm.vue
+++ src/components/TimelineElementForm.vue
@@ -9,7 +9,7 @@
       <validation-provider
         v-slot="{ errors }"
         name="Title"
-        rules="required|max:32|min:6"
+        rules="required|max:32|min:4"
       >
         <v-text-field
           v-model="newElement.title"
@@ -104,13 +104,14 @@ export default {
       }
 
       if (this.props && this.props.element) {
-        //TODO: update element logic
+        //TODO: Update Element logic
         return;
       }
       try {
         this.newElement.id = uuidv4();
-        console.log(this.newElement);
         this.addElement(this.newElement);
+        this.clear();
+        //TODO: Notifications logic
       } catch (error) {
         console.log(error);
       }

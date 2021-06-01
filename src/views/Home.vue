5e7463b928432bb718c7d46972bcd3557c965b93
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-01 19:05:38 +0200

    Add TimelineElementsList component in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <timeline-element-add />
+  <main>
+    <timeline-element-add />
+    <timeline-elements-list />
+  </main>
 </template>
 
 <script>
 import TimelineElementAdd from "../components/TimelineElementAdd.vue";
+import TimelineElementsList from "../components/TimelineElementsList.vue";
 
 export default {
   name: "Home",
 
   components: {
     TimelineElementAdd,
+    TimelineElementsList,
   },
 };
 </script>

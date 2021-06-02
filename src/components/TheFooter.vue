165c258e168e357ff0be1b61c92c3711d09363a4
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-02 21:29:03 +0200

    Add TheFooter component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — Made with 💙 by Gabriel Ron
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<style></style>

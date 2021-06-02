42b88e877c4e60b56ded56fcf984da5fa743decd
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-02 18:51:26 +0200

    Add TheHeader component in App root element

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <v-app>
-    <v-main>
+    <the-header />
+    <v-main class="my-16">
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+
 export default {
+  components: { TheHeader },
   name: "App",
 
   data: () => ({

b5f9b66ec249649fd3d4204725915f6ede0d75e4
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-02 21:28:22 +0200

    Add time property in Element entity

--- src/components/utils/generateNotification.js
+++ src/components/utils/generateNotification.js
@@ -7,12 +7,14 @@ const generateNotification = (
     return {
       type: "error",
       content: errorContent,
+      time: new Date().toLocaleTimeString(),
     };
   }
 
   return {
     type: "success",
     content: successContent,
+    time: new Date().toLocaleTimeString(),
   };
 };
 

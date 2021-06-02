eea4c0fdaa23e7d57eee0dde3ab409192c55959d
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-02 21:31:57 +0200

    Add theFooter in App root element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <v-app>
     <the-header />
-    <v-main class="my-16">
+    <v-main class="mt-16">
       <router-view />
     </v-main>
+    <the-footer />
   </v-app>
 </template>
 
 <script>
+import TheFooter from "./components/TheFooter.vue";
 import TheHeader from "./components/TheHeader.vue";
 
 export default {
-  components: { TheHeader },
+  components: {
+    TheFooter,
+    TheHeader,
+  },
   name: "App",
 
   data: () => ({

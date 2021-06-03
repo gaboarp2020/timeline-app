1ebd3b7af8dd7c387916097abe0ca321adac81fd
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-03 07:35:43 +0200

    Add TimelinePage layout

---
+++ src/layouts/TimelinePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-16">
+    <timeline-element-form-add />
+    <timeline-elements-list />
+  </div>
+</template>
+
+<script>
+import TimelineElementFormAdd from "../components/TimelineElementFormAdd.vue";
+import TimelineElementsList from "../components/TimelineElementsList.vue";
+
+export default {
+  components: {
+    TimelineElementFormAdd,
+    TimelineElementsList,
+  },
+  name: "TimelinePage",
+};
+</script>
+
+<style></style>
